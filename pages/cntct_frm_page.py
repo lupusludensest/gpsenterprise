5e11541a70ef2fd89a9afcec485b5b62098cba2b
LupusLudensEst <gurovvic@gmail.com>
2020-06-07 19:29:53 -0400

    update dt07 june 2020

--- pages/cntct_frm_page.py
+++ pages/cntct_frm_page.py
@@ -1,11 +1,12 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import Page
+from time import sleep
 
 class ContactFormPage(Page):
 
     # locators
-    CNTCT_BTN = (By.XPATH, "//li[@class='active menu-item menu-contact'][1]") # "menu-header-navigation-right"# "li.active.menu-item.menu-contact" # "//a[@aria-current='page']" # "//a[@href='https://analyticpartners.com/contact-us/']"
-    FRST_NM_FLD = (By.ID, "firstname-9d6686cd-0cff-4088-b7e6-860da1461c18")
+    CNTCT_BTN = (By.XPATH, "//ul[@id='menu-header-navigation-right']//a[contains(text(),'Contact')]")
+    FRST_NM_FLD = (By.ID, "hs-form-iframe-0")
     LST_NM_FLD = (By.ID, "lastname-9d6686cd-0cff-4088-b7e6-860da1461c18")
     CMPN_NM_FLD = (By.ID, "company-9d6686cd-0cff-4088-b7e6-860da1461c18")
     JB_TTL_FLD = (By.ID, "jobtitle-9d6686cd-0cff-4088-b7e6-860da1461c18")
@@ -20,30 +21,38 @@ class ContactFormPage(Page):
         Click on Contact button
         """
         self.click(*self.CNTCT_BTN)
-
+        sleep(4)
 
     def inpt_frst_nm(self, text):
         """
         User enters First name
         """
+        sleep(4)
+        self.driver.switch_to.frame(self.driver.find_element_by_xpath("//iframe[@id='hs-form-iframe-0']"))
         self.input_text(text, *self.FRST_NM_FLD)
+        # self.driver.switch_to.frame(self.driver.find_element_by_css_selector("iframe#hs-form-iframe-0.hs-form-iframe")
+        # self.input_text(text, *self.FRST_NM_FLD)
+        sleep(4)
 
     def inpt_scnd_nm(self, text):
         """
         User enters Last name
         """
         self.input_text(text, *self.LST_NM_FLD)
+        sleep(4)
 
     def inpt_cmpn_nm(self, text):
         """
         User enters Company name
         """
         self.input_text(text, *self.CMPN_NM_FLD)
+        sleep(4)
 
     def inpt_jb_ttl(self, text):
         """
         User enters Job Title
         """
+        sleep(4)
         self.input_text(text, *self.JB_TTL_FLD)
 
     def inpt_eml(self, text):
@@ -51,24 +60,28 @@ class ContactFormPage(Page):
         User enters Email
         """
         self.input_text(text, *self.EML_FLD)
+        sleep(4)
 
     def inpt_phn_nmbr(self, text):
         """
         User enters Phone number
         """
         self.inpt_phn_nmbr(text, *self.PHN_NMBR_FLD)
+        sleep(4)
 
     def inpt_msg(self, text):
         """
         User enters into the Message field
         """
         self.inpt_phn_nmbr(text, *self.MSG_FLD)
+        sleep(4)
 
     def clck_agree_tick(self):
         """
         User agrees with I agree to receive other communications from Analytic Partners
         """
         self.click(*self.AGREE_TICK)
+        sleep(4)
 
     def txt_hr(self, text):
         """
